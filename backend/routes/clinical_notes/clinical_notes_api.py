6e86ae93d76589f522ff36c00054c7734fd25b88
Sathira Katugaha <sathirakatugaha@Sathiras-MacBook-Pro-2.local>
2024-12-31 14:41:27 -0500

    fix typos and assert output format in system prompt

--- backend/routes/clinical_notes/clinical_notes_api.py
+++ backend/routes/clinical_notes/clinical_notes_api.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
-load_dotenv() 
+
+load_dotenv()
 
 import logging
 import os
@@ -33,38 +34,37 @@ logging.basicConfig(level=logging.INFO)
 @router.post("/", response_model=ClinicalNoteRes)
 async def generate_clinical_notes(request: ClinicalNoteReq):
     logging.info(f"Received request to generate session notes: {request.dict()}")
-    
+
     system_prompt = (
-         "You are an experienced ABA therapist specializing in autism care." + 
-                 " You are generating professional notes using inputs of therapy session observations and therapy" + 
-                 " session parameters. Session observations are quick notes written by therapists about the therapy" +
-                 " session. Session parameters are categorical or quantifiable features of the session such as"+
-                 " duration or type"
+        "You are an experienced ABA therapist specializing in autism care."
+        + " You are generating professional notes using inputs of therapy session observations and therapy"
+        + " session parameters. Session observations are quick notes written by therapists about the therapy"
+        + " session. Session parameters are categorical or quantifiable features of the session such as"
+        + " duration or type. Output your notes in markdown format."
     )
-    
+
     user_prompt = "Hi, can you generate some professional ABA notes for me using the following observations and session parameters?\nMy observations are:"
 
     for observation in request.quick_observations:
-        user_prompt.append(f"\n- {observation}")
+        user_prompt += f"\n- {observation}"
 
-    user_prompt.append("\nMy session parameters are:")
+    user_prompt += "\nMy session parameters are:"
 
-    for (parameter, value) in request.session_parameters.items():
-        user_prompt.append(f"\n- {parameter}: {value}")
+    for parameter, value in request.session_parameters.items():
+        user_prompt += f"\n- {parameter}: {value}"
 
     try:
         logging.info(f"Generating chat response from Open AI model: {OPEN_AI_MODEL}")
         response = client.chat.completions.create(
-            model=OPEN_AI_MODEL,  
+            model=OPEN_AI_MODEL,
             messages=[
-                {"role": "system", "content": system_prompt
-                 },
+                {"role": "system", "content": system_prompt},
                 {"role": "user", "content": user_prompt},
             ],
             temperature=0.7,
             max_tokens=500,
         )
-       
+
         ai_notes = response.choices[0].message.content.strip()
         logging.info(f"Received the following response from OpenAI {ai_notes}")
         return {"notes": ai_notes}
